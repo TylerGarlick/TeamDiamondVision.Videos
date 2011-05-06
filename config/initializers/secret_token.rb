18bca4ef45898d42240aa26ea24a703ee8487ad1
Tyler Garlick <tjgarlick@gmail.com>
2011-05-05 22:07:39 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tdvvideo::Application.config.secret_token = 'cdae267428e91344296ec73bee1e01034b590db92b6c81841f29abddffde0d2ea3d42e86ff6a088674be9a76fd135e096c2ba2ce1805c12372758874d10d841f'
